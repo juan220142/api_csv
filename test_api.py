b45377969a4e5b42fa77407506d8e5befbefa439
juan220142 <61586696+juan220142@users.noreply.github.com>
2024-01-10 14:13:39 -0500

    Delete test_api.py

--- test_api.py
+++
@@ -1,9 +0,0 @@
-import json
-from flask_testing import TestCase
-from main import app
-
-class MyTest(TestCase):
-        def test_get_users(self):
-                response = self.client.get('/')
-                self.assertEqual(response.status_code, 200)
-                self.assertIn('users', json.loads(response.data))
